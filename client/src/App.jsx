81538e04280cb704edbe1c06e1958e34742f846d
Shrishaan Bajracharya <shreetheshan@gmail.com>
2025-11-10 21:55:05 +0545

    Changed tailwind base vars

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ import React from 'react'
 const App = () => {
   return (
     <div className="p-6">
-      <h1 className="text-2xl font-bold mb-4">App</h1>
+      <h1 className="text-2xl font-bold mb-4">SHRISHAAN</h1>
 
       <Button>Shadcn Button</Button>
     </div>
