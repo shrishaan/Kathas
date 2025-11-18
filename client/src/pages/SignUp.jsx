5b2c91729edf15da5a0dbc0d2f7a32175ea48cee
Shrishaan Bajracharya <shreetheshan@gmail.com>
2025-11-18 10:29:26 +0545

    Fixed SignUp error

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -22,7 +22,7 @@ const SignUp = () => {
         name: z.string().min(3,'Name must be at least 3 characters long.'),
         email: z.string().email(),
         password: z.string().min(8, "Password must be at least 8 characters long."),
-        confirmPassword: z.string().refine(data.password == data.confirmPassword, 'Passwords do not match.'),
+        confirmPassword: z.string().refine( data => data.password == data.confirmPassword, { message: 'Passwords do not match.', path:['confirmPassword']}),
       });
     
       const form = useForm({
