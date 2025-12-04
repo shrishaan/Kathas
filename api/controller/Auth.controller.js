7f688a46ae42db48ecb867e31fdf67a677137cd0
Shrishaan Bajracharya <shreetheshan@gmail.com>
2025-12-04 12:53:09 +0545

    correct response key for logged-in user in Login function

--- api/controller/Auth.controller.js
+++ api/controller/Auth.controller.js
@@ -64,7 +64,7 @@ export const Login = async (req, res, next) => {
 
         res.status(200).json({
             success:true,
-            newUser,
+            user:newUser,
             message: 'User logged in successfully'
         })
 
