a64899f8bec47a774d5c58a36dd49c994b5e07f1
Shrishaan Bajracharya <shreetheshan@gmail.com>
2025-12-16 22:56:05 +0545

    Added dropdown menu and avatar

--- client/src/components/ui/Topbar.jsx
+++ client/src/components/ui/Topbar.jsx
@@ -1,30 +1,69 @@
-import React from 'react'
-import logo from "@/assets/images/logo-white.png"
-import { Link } from 'react-router-dom';
-import { Button } from './Button';
+import React from "react";
+import logo from "@/assets/images/logo-white.png";
+import { Link } from "react-router-dom";
+import { Button } from "./Button";
 import { MdLogin } from "react-icons/md";
-import SearchBox from './SearchBox';
-import { RouteSignIn } from '@/helpers/RouteName';
+import SearchBox from "./SearchBox";
+import { RouteSignIn } from "@/helpers/RouteName";
+import { useSelector } from "react-redux";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuGroup,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuPortal,
+  DropdownMenuSeparator,
+  DropdownMenuShortcut,
+  DropdownMenuSub,
+  DropdownMenuSubContent,
+  DropdownMenuSubTrigger,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import usericon from "@/assets/images/user.png";
 
 const Topbar = () => {
+  const user = useSelector((state) => state.user);
   return (
-    <div className='flex justify-between items-center h-16 fixed w-full z-20 bg-white px-5 border-b'>
+    <div className="flex justify-between items-center h-16 fixed w-full z-20 bg-white px-5 border-b">
       <div>
         <img src={logo} />
       </div>
-      <div className='w-[500px]'>
+      <div className="w-[500px]">
         <SearchBox />
       </div>
       <div>
-        <Button asChild className='rounded-full'>
-          <Link to={RouteSignIn} >
-          <MdLogin />
-          Sign In
-          </Link>
-        </Button>
+        {!user.isLoggedIn ? (
+          <Button asChild className="rounded-full">
+            <Link to={RouteSignIn}>
+              <MdLogin />
+              Sign In
+            </Link>
+          </Button>
+        ) : (
+          <DropdownMenu>
+            <DropdownMenuTrigger>
+              <Avatar>
+                <AvatarImage src={user.user.Avatar || usericon} />
+                <AvatarFallback>CN</AvatarFallback>
+              </Avatar>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuLabel>
+                <p>{user.user.name}</p>
+                <p className="text-sm">{user.user.email}</p>
+              </DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem>
+                <Link to="">Profile</Link>
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        )}
       </div>
     </div>
-  )
-}
+  );
+};
 
 export default Topbar;
